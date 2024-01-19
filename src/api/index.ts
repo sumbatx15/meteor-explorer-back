8f7dc930eb6b2a54f63782fc7f3834044896dae2
sumbatx15 <sumbat15@gmail.com>
2024-01-19 10:31:39 +0200

    vercel test

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,12 @@ import asteroidsYears from "./asteroids/years";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json({
+    message: "API - 👋🌎🌍🌏",
+  });
+});
+
 router.use("/asteroids/query", asteroidsQuery);
 router.use("/asteroids/years", asteroidsYears);
 
